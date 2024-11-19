f2ba67fb37821c53876ff43e30eba720ca26792a
Kinza-Developer <ubaidullahmayub203@gmail.com>
2024-11-19 14:48:30 +0500

    Fixed deployment issues

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className='hidden lg:block'></div>
         <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center pt-18'>
           <div>
-            <p data-aos="zoom-in-up">I'm</p>
+            <p data-aos="zoom-in-up">I am</p>
             <p data-aos="zoom-in-up">Hafiza</p>
             <p data-aos="zoom-in-up">Kinza</p>
           </div>
